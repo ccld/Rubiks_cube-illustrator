59167a6dd8059f7017a52af5e0750997bb042cc8
ccld <claude.cornen@club-internet.fr>
2025-03-10 17:23:30 +0100

    Create parse_algorithm.py

---
+++ structure/parse_algorithm.py
@@ -0,0 +1,49 @@
+"""
+Function to parse a movement notation string like 'FLRU2Lr' into a format that can be used with the perform_moves function. Here's how I'll interpret the notation:
+
+The parser follows standard Rubik's cube notation:
+Letters without modifiers (F, L, R, U) represent clockwise rotations of that face (Front, Left, Right, and Up faces)
+A number after a letter means repeat that move that many times (U2 = perform U twice)
+Lowercase letters (like r) represent counterclockwise rotations
+
+Implementation will:
+Parse 'FLRU2Lr' into the moves: (F,cw), (L,cw), (R,cw), (U,cw), (U,cw), (L,ccw), (R,ccw)
+"""
+
+def parse_move_notation(notation_string):
+    """
+    Parse a move notation string into a list of (face, direction) tuples for perform_moves.
+    
+    :param notation_string: String like 'FLRU2Lr' where:
+                            - Uppercase letters are clockwise rotations
+                            - Lowercase letters are counterclockwise rotations
+                            - Numbers after a letter repeat that move that many times
+    :return: List of (face, direction) tuples
+    """
+    moves = []
+    i = 0
+    
+    while i < len(notation_string):
+        # Get the current character (face)
+        char = notation_string[i]
+        i += 1
+        
+        # Determine the face and direction
+        if char.isupper():
+            face = char
+            direction = 'cw'  # Uppercase is clockwise
+        else:
+            face = char.upper()
+            direction = 'ccw'  # Lowercase is counterclockwise
+        
+        # Check if the next character is a number (repetition)
+        repetitions = 1
+        if i < len(notation_string) and notation_string[i].isdigit():
+            repetitions = int(notation_string[i])
+            i += 1
+        
+        # Add the move(s) to the list
+        for _ in range(repetitions):
+            moves.append((face, direction))
+    
+    return moves
