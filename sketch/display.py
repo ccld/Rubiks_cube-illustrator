5f0f6b6f3eb570d49038d57a0354edc96ee33bfe
ccld <claude.cornen@club-internet.fr>
2025-03-18 09:16:32 +0100

    Update display.py

--- sketch/display.py
+++ sketch/display.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# =====================================================================
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
