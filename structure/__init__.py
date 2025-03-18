3000010a2917e4247cb02644c1bb0942db7dd5d5
ccld <claude.cornen@club-internet.fr>
2025-03-18 10:11:49 +0100

    Update __init__.py

--- structure/__init__.py
+++ structure/__init__.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# =====================================================================
+# Makes the sketch directory a Python package
+# This allows importing from sketch modules
+# =====================================================================
 from parse_algorithm import *
 from moves import *
 from execute-move_sequence import *
