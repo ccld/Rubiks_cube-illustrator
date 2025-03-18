2c872a8943d86549bf8a5bb61879b7be92308cb3
ccld <claude.cornen@club-internet.fr>
2025-03-18 10:09:47 +0100

    Delete sketch/--init--.py

--- sketch/--init--.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-# Makes the sketch directory a Python package
-# This allows importing from sketch.display and sketch.video
-# =====================================================================
