d20443a0467369de6acf6075311a52e4572003cc
ccld <claude.cornen@club-internet.fr>
2025-03-18 09:18:12 +0100

    Update --init--.py

--- sketch/--init--.py
+++ sketch/--init--.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# Makes the sketch directory a Python package
+# This allows importing from sketch.display and sketch.video
 # =====================================================================
