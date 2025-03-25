8c163dfcd11500b225af3ad8da6e2a0d949ecb31
ccld <claude.cornen@club-internet.fr>
2025-03-25 08:10:42 +0100

    Update Rubiks_illustrator.py

--- Rubiks_illustrator.py
+++ Rubiks_illustrator.py
@@ -1,4 +1,7 @@
+"""Rubiks_illustrator.py: a Rubik's cube perspective image simulator tool using overlapping Venn diagram
+"""
 #!/usr/bin/env python3
+# ===============================
 import sys, getopt
 
 from structure import *
@@ -29,7 +32,7 @@ def Rubiks(twists):
         face = char
         
         # For slice turns (M, E, S), the default direction is counterclockwise
-        if face in "MES":
+        if face in "BDLMES":
             direction = 'ccw'  # Default for M, E, S is counterclockwise
         else:
             direction = 'cw'  # Default for all other moves is clockwise
@@ -62,7 +65,7 @@ def main(argv):
       if o in ("-v", "--verbose"):
         vb = True
       elif o in ("-x", "--upfront"):
-          up = a               
+          fur = a               
       elif o in ("-t", "--twist"):
           turns = a               
       elif o in ("-c", "--clean"):
@@ -72,29 +75,27 @@ def main(argv):
     
     moves_list = Rubiks(turns)
     print(f"Parsed {len(moves_list)} moves: {moves_list}")
-    print('hue', up)    
+    print('FUR', up)    
     corner=[]
       
     # Initialize the cube
-    cube, face_colors, corner = framesetup.initialize_cube(orientation_code = up) 
+    cube, face_colors, corner = framesetup.initialize_cube(orientation_code = fur) 
     print('corner view  =', corner)
     initial_points, initial_colors = framesetup.generate_initial_points(corner)
-    display.create_rubiks_diagram(initial_points, initial_colors,0,'start')
+    display.create_rubiks_diagram(initial_points, initial_colors,fur ,'start')
     # Test faces 4 clockwise
     points_after_url, colors_after_url = perform_moves(
         initial_points, initial_colors, 
         moves_list
         ) 
-    display.create_rubiks_diagram(points_after_url, colors_after_url, n, \
-            'Group Theory')
+    display.create_rubiks_diagram(points_after_url, colors_after_url, n, subtext)
 
     return 
 
 if __name__ == "__main__":
     n=3 # for diagraam subtitle 
-    number=len(sys.argv)
-    print ('The Number of arguments:', number, 'arguments.')
-    print ('Argument List:', sys.argv[1:])        
+    print ('Argument List:', sys.argv[1:])   
+    subtext = 'Fig'  # caption
     main(sys.argv[1:])
 
 
