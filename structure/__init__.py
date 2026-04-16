43337e2f7df9eed3866d678a6671ecc85f060374
ccld <claude.cornen@club-internet.fr>
2026-04-16 21:48:15 +0200

    Import framesetup in structure package
    Added import statement for framesetup module.

--- structure/__init__.py
+++ structure/__init__.py
@@ -4,7 +4,10 @@
 # Makes the strusture directory a Python package
 # This allows importing from structure modules
 # =====================================================================
+'''
 from parse_algorithm import *
 from moves import *
 from execute-move_sequence import *
 from illustrator import *
+'''
+from framesetup import *
